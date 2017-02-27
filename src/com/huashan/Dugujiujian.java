bcc814940899311a34b1b828040469e06342f0c7
liuchuan8 <15032552087@163.com>
2017-02-27 23:55:33 +0800

    update 04

--- src/com/huashan/Dugujiujian.java
+++ src/com/huashan/Dugujiujian.java
@@ -3,5 +3,6 @@ package com.huashan;
 public class Dugujiujian {
 	public static void main(String[] args) {
 		System.out.println("hello ,duguijiujian");
+		System.out.println("helllo,AAAAAAAAAAAAAa");
 	}
 }
