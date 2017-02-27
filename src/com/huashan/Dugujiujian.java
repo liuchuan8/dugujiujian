eb5b8e8e6021b87fd87362972b3ad86f3d548e40
liuchuan8 <15032552087@163.com>
2017-02-27 23:11:20 +0800

    update 02

--- src/com/huashan/Dugujiujian.java
+++ src/com/huashan/Dugujiujian.java
@@ -3,5 +3,7 @@ package com.huashan;
 public class Dugujiujian {
 	public static void main(String[] args) {
 		System.out.println("hello ,duguijiujian");
+		System.out.println("hello ,duguijiujian1");
+		System.out.println("hello ,duguijiujian2");
 	}
 }
